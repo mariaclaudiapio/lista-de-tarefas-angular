5f09257bda023769611e722888d33a3d7852f39e
MariaClaudia1982 <mariaclaudiapio@gmail.com>
2022-09-14 22:42:04 -0300

    incluindo função de deletar tarefas

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
+  public taskList: Array<TaskList> = [
+    { task: "Minha nova Task", checked: true },
+    { task: "Minha nova Task 2", checked: false },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm("Você deseja, realmente, apagar tudo?")
+    if(confirm) {
+      this.taskList = [];
+    }
+    this.taskList = [];
+  }
+
 
 }
